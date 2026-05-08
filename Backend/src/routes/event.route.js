bb805150907f4b262d0985dc6060989a366ae287
Fiderana-17 <hei.fiderana@gmail.com>
2026-05-08 11:16:04 +0300

    feat(event): implement get all event API route

--- Backend/src/routes/event.route.js
+++ Backend/src/routes/event.route.js
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import { getAllEvents, getEventById, createEvent, updateEvent, deleteEvent } from "../controllers/event.controller.js";
+import { getAllEvents } from "../controllers/event.controller.js";
 
 const router = Router();
 
 router.get("/", getAllEvents);
-router.get("/:id", getEventById);
-router.post("/", createEvent);
-router.put("/:id", updateEvent);
-router.delete("/:id", deleteEvent);
+
 
 export default router;
