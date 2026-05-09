3a32f9181a7cf9baae6c66d0363d657c81f19d42
mena <tsikiniainatsilavina@gmail.com>
2026-05-09 10:46:23 +0300

    feat/speaker : add way into server.js for speaker

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,14 +3,17 @@ import express from "express";
 import cors from "cors";
 
 import eventRoutes from "./routes/event.route.js";
-
+import speakerRoutes from "./routes/speakers.route.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
+
 app.use("/api/events", eventRoutes);
+app.use("/api/speakers", speakerRoutes);
 
 app.get("/", (req, res) => res.send("EventFlow API is running."));
 
