b42c52e0e4a2309a04d74490004777da2de0d0e4
Mandrindra <hei.mandrindra@gmail.com>
2026-05-19 12:16:47 +0300

    feat: add useNow hook

---
+++ Frontend/src/hooks/useNow.ts
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export function useNow(intervalMs = 10_000): Date {
+  const [now, setNow] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => setNow(new Date()), intervalMs);
+    return () => clearInterval(id);
+  }, [intervalMs]);
+  return now;
+}
